d6c9da68b3af55dc52afcb1699f7e949bf8a2935
Maxim Fomin <aten57@yandex.ru>
2017-09-09 02:37:22 +0300

    add project

--- Conv.py
+++ Conv.py
@@ -1,37 +1,78 @@
 from tkinter import *
 
 root = Tk()
-root.geometry('600x600')
+root.geometry('800x800')
+root.title('Создание выпуклой оболочки')
 
-canv = Canvas(bg='gray')
+canv = Canvas(bg='#00E39F')
 canv.pack(fill=BOTH, expand=1)
+canv.pack()
+
 t = []
+myconst = 0
+
+def rotate(A, B, C):  # скалярное произведение
+    return (B[0] - A[0]) * (C[1] - B[1]) - (B[1] - A[1]) * (C[0] - B[0])
+
+
+def jarvismarch(A):  # алгоритм Джарвиса
+    n = len(A)
+    minPoint = A[0][0]
+    k = 0
+    P = A
+    for i in range(1, n):
+        if A[i][0] < minPoint:
+            minPoint = A[i][0]
+            k = i
+    leftPoint = A[k]
+    P.remove(P[k])
+    H = [leftPoint]
+    P.append(leftPoint)
+    while True:
+        right = 0
+        for i in range(1, len(P)):
+            if rotate(H[-1], P[right], P[i]) < 0:
+                right = i
+        if P[right] == H[0]:
+            break
+        else:
+            H.append(A[right])
+            P.remove(P[right])
+    return H
 
 def click(event):
-    global prev_x, prev_y  # изменения в этих переменных будут сохранены и после окончания работы функции
+    global myconst, t
+    if myconst > 0:
+        t = []
+        myconst = 0
+        canv.delete("all")
     x = event.x
     y = event.y
-    r = 50
     canv.create_oval([x + 5, y + 5], [x - 5, y - 5], activefill="green", activeoutline="yellow", outline="red",
                      fill="pink")
-    if prev_x:  # если в переменной prev_x есть значение (не None)
-        canv.create_line(x, y, prev_x, prev_y, width=3, fill="blue")
-    # запомнить координаты этого щелчка как "предыдущие" для следующего
-    prev_x = x
-    prev_y = y
     t.append([x, y])
-    print(t)
 
+def myfunc(event):
+    global t
+    global myconst
+    try:
+        w = jarvismarch(t)
+        for i in range(len(w)):
+            canv.create_oval([w[i][0] + 5, w[i][1] + 5], [w[i][0] - 5, w[i][1] - 5], activefill="red", activeoutline="yellow", outline="red",
+                             fill="yellow")
 
-# до первого щелчка никаких "предыдущих" нет
-prev_x = None
-prev_y = None
-# lbl = Label(root, width = 180)
-# lbl.configure(text = "Hello")
-# lbl.pack()
-# ent = Entry(root)
-# ent.pack()
-# ent.bind('<Return>',caption)
-canv.bind('<1>', click)
+        canv.create_line(w[0:len(w)], fill='blue')
+        canv.create_line(w[len(w)-1], w[0], fill='blue')
+        myconst += 1
+        t = []
+    except:
+        canv.delete("all")
+
+w = Label(canv, text="Приветствую тебя в моей программе!\n Расставляй точки мышкой, для построения\n оболочки нажми Enter на клавиатуре :)", font=("Helvetica", 26), fg="orange", bg='#00E39F')
+
+w.pack()
+root.bind('<1>', click)
+root.bind('<Return>', myfunc)
+
 
 mainloop()
